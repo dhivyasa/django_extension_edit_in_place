3aeb8f11425e1875c97cab7aba3c93e75e42c48c
Dhivya Sivasubramanian <dhivya.s.a@gmail.com>
2013-05-10 09:40:56 -0400

    Add jquery,js plugin

--- django_extension_project/example_usage_project/urls.py
+++ django_extension_project/example_usage_project/urls.py
@@ -1,7 +1,15 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
 from settings import root_directory
 
 urlpatterns = patterns( '' ,
+(r'^files/(?P<path>.*)$', 'django.views.static.serve',
+             {'document_root': root_directory + '/static'}),
+(r'^example_app/$','example_app.views.index'),
+(r'^example_app/(?P<poll_id>\d+)/$','example_app.views.details'),
 (r'^edit_field/',include('edit_field.urls')),        
 (r'^edit_field/static/(?P<path>.*)$', 'django.views.static.serve',
   {'document_root':root_directory + '/edit_field/static'}))            
