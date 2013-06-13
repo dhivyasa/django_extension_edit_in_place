efefc8264fd988d4ff38947b0740ee58e23b1a5d
Dhivya Sivasubramanian <dhivya.s.a@gmail.com>
2013-06-13 07:42:15 -0400

    use render_to_response shortcut

--- django_extension_project/example_usage_project/example_app/views.py
+++ django_extension_project/example_usage_project/example_app/views.py
@@ -2,12 +2,11 @@
 
 from django.http import HttpResponse
 from example_app.models import Poll
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 def index(request):
     return HttpResponse("Hello world . You are in example_app index")
 
-
 def details(request,poll_id):
     poll = Poll.objects.get(pk=poll_id)
-    return render(request,'example_app/detail.html',{'poll':poll}) 
+    return render_to_response('example_app/detail.html',{'poll':poll}) 
